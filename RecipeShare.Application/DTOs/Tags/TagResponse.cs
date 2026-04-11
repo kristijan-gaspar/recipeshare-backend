9c9e129be771b05d91c1f4d6dbeea51a34ae618e
lukad <dujam.luka@gmail.com>
2026-04-11 19:50:14 +0200

    feat: add Category and Tag entities with DTOs

---
+++ RecipeShare.Application/DTOs/Tags/TagResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeShare.Application.DTOs.Tags;
+
+public class TagResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = "tag";
+
+    public int RecipeCount { get; set; }
+}
