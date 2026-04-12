5f9f8a104e2b11e5928f926364e7da0ba756383c
lukad <dujam.luka@gmail.com>
2026-04-13 01:06:04 +0200

    feat: add admin DTOs with extended fields for categories and tags"

---
+++ RecipeShare.Application/DTOs/Tags/AdminTagResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeShare.Application.DTOs.Tags;
+
+public class AdminTagResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = String.Empty!;
+    public string Type { get; set; } = "tag";
+    public int RecipeCount { get; set; } = 0;
+    public DateTime CreatedaAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsActive { get; set; }
+}
