54bf69a13a97578b228ac228f5370875f43ccf7a
lukad <dujam.luka@gmail.com>
2026-04-13 21:22:57 +0200

    feat: add user search DTOs and generic paged response

---
+++ RecipeShare.Application/Common/PagedResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RecipeShare.Application.Common;
+
+public class PagedResponse<T>
+{
+    public List<T> Items { get; set; } = new();
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public bool HasNextPage { get; set; }
+}
